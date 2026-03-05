2741d0c8dc6cbde538451a7874e443518b352622
eduardossoares <eduardo21.net@gmail.com>
2026-03-04 21:38:40 -0300

    chore: lint check

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -12,26 +12,26 @@ import { App } from "./App";
 const elem = document.getElementById("root");
 
 if (!elem) {
-	throw new Error("Root element not found");
+  throw new Error("Root element not found");
 }
 
 const app = (
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 if (import.meta.hot) {
-	// With hot module reloading, `import.meta.hot.data` is persisted.
-	let root: Root | undefined = import.meta.hot.data.root;
+  // With hot module reloading, `import.meta.hot.data` is persisted.
+  let root: Root | undefined = import.meta.hot.data.root;
 
-	if (!root) {
-		root = createRoot(elem);
-		import.meta.hot.data.root = root;
-	}
+  if (!root) {
+    root = createRoot(elem);
+    import.meta.hot.data.root = root;
+  }
 
-	root.render(app);
+  root.render(app);
 } else {
-	// The hot module reloading API is not available in production.
-	createRoot(elem).render(app);
+  // The hot module reloading API is not available in production.
+  createRoot(elem).render(app);
 }
